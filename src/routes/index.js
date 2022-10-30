1728840824ad7a70609970aeeb1546575923b47d
LPH1110 <lephuhaotech1124@gmail.com>
2022-10-30 12:01:37 +0700

    Updates

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import { Home } from '~/pages';
-import { DefaultLayout } from '~/layouts';
-
-const publicRoutes = [{ id: 0, path: '/', component: Home, layout: DefaultLayout }];
-
-export { publicRoutes };
